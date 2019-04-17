ec6dbff386df9df944819fce1ee268301882911c
wim-web <rootuser.main+github@gmail.com>
2019-04-17 15:44:44 +0900

    first commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>PortFolio</title>
+
+    <script src="{{ mix('js/app.js') }}" defer></script>
+
+    <link href="{{ mix('css/app.css') }}" rel="stylesheet">
+</head>
+<body>
+    <div id="app"></div>
+</body>
+</html>
